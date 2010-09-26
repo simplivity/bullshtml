820c1f54d2df4247ae593395a0e9c5672b99db45
junoyoon <junoyoon@c474294e-3f17-11de-ba0d-4f26e5d835f1>
2010-09-26 12:36:16 +0000

    remove unnecessary files

--- trunk/test/com/junoyoon/BullsHtmlTest.java
+++ trunk/test/com/junoyoon/BullsHtmlTest.java
@@ -4,9 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 
-import org.antlr.stringtemplate.StringTemplate;
 import org.jdom.JDOMException;
 import org.junit.Test;
 
@@ -17,57 +15,13 @@ import static org.junit.Assert.assertThat;
 public class BullsHtmlTest {
 	@Test
 	public void testProcessInternal() throws JDOMException, IOException {
-		
-		System.out.println(new File("c:").getCanonicalFile().getPath());;
-		
 		File file = new File("test/coverage.xml");
 		FileReader reader = new FileReader(file);
-		try {
-			BullsHtml bullsHtml = new BullsHtml();
-			bullsHtml.processInternal(reader);
-			for (SrcDir src : bullsHtml.srcMap.values()) {
-				System.out.println(src.path);
-				for (Src child : src.child) {
-					System.out.println("--" + child.path);
-				}
-			}
-			System.out.println("---");
-			for (Src src : bullsHtml.srcFileList) {
-				System.out.println(src.path);
-			}
-			System.out.println("---");
-			for (Src src : bullsHtml.baseList) {
-				System.out.println(src.path);
-			}
-			new File("output").mkdir();
-			bullsHtml.generateHtml(new File("output"));
-			bullsHtml.copyResources("output");
-			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
-	public void testStringTemplate() {
-		StringTemplate a = new StringTemplate("$b:{a|$a$, }$");
-		a.setAttribute("b", new ArrayList<Integer>() {{ add(1); add(2);}});
-		System.out.println(a.toString());;
-		a.reset();
-		a.setAttribute("", new ArrayList<Integer>() {{ add(3); add(22);}});
-		System.out.println(a.toString());;
-		
-		StringTemplate tem = new StringTemplate("$a.size$");
-		tem.setAttribute("a", new ArrayList<Integer>() {{ add(3); add(22);}});
-		
-		System.out.println(tem.toString());
-		
-	}
-	
-	@Test
-	public void testWow() {
-		StringTemplate a = new StringTemplate("$b.size()");
+		BullsHtml bullsHtml = new BullsHtml();
+		bullsHtml.processInternal(reader);
+		new File("output").mkdir();
+		bullsHtml.generateHtml(new File("output"));
+		bullsHtml.copyResources("output");
 	}
 
 	@Test
